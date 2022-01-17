83bacdcef9b4928ec351ba48254b58ad59f51fc2
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-01-17 15:41:34 +0100

    Update App.vue
    correction font

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
-  <img id="logo" alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="la ce message est ou?" />
+  <div id="app">
+    <router-view/>    
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+  name: 'App',
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-#logo {
-  width: 100px;
-  height: 150px;
+<style>
+@font-face {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-weight: normal;
+    font-style: normal;
 }
 </style>
