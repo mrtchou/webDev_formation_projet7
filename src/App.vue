9ce04d93a3b7c3515ea0cb4629871b345a8a95f4
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-01-17 15:38:10 +0100

    Add files via upload
    first commit for application reseau social

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img id="logo" alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="la ce message est ou?" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#logo {
+  width: 100px;
+  height: 150px;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>About a Propos</h1>
+</template>
