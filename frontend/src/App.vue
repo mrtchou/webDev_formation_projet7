97cd17cb406ebf5b4302f8ac2fc5be7fbd76aaa2
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-01-28 17:38:45 +0100

    Update App.vue
    amelioration page accueil de l'application: creation lien de connexion, inscription, un footer avec date dynamique, creation menu temporaire, et partie css avec une ressamblance couleur proche twitter

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,76 @@
 <template>
   <div id="app">
-    <router-view/>    
+    <div id="nav">
+      <router-link to="/">Accueil</router-link> |
+      <router-link to="/account">Mon Compte</router-link> |
+      <router-link to="/forum">Forum</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-}
-</script>
+
+
+
 
 
 <style>
-@font-face {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    font-weight: normal;
-    font-style: normal;
+* {
+  background: #40bbea;
+}
+
+html {
+  height: 100%;
+}
+body {
+  min-height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+button, input, textarea {
+  background: #d14454;
+  color: #fff;
+}
+
+button {
+  padding: 5px;
+  cursor: pointer;
+}
+
+#app {
+  font-family: sans-serif;
+  font-weight: 500;
+  text-align: center;
+  color: #FFF;
+}
+
+#nav {
+  padding: 30px;
+}
+
+.form-div {
+  margin: 20px;
+  label {
+    display: inline-block;
+    width: 170px;
+    &.keepConnexion {
+      text-align: left;
+      margin-left: 10px;
+    }
+    &.search {
+      width: 100px;
+    }
+  }
+  .submit {
+    font-size: 1rem;
+    cursor: pointer;
+    padding: 5px;
+  }
+}
+
+.button {
+  font-size: 0.85rem;
+  margin: 20px;
 }
 </style>
