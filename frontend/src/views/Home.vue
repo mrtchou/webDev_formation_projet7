cfbda3fb63ebbe3d94ad082a09d0458b0c49fe84
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-01-29 19:24:26 +0100

    Update Home.vue
    deplacement footer sur page Vue principale

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="home">
-    <header>
-     
-    </header>
-    <main>
-      <h1>Bienvenue dans le réseau social de votre societé</h1>
-      
-      <p class="welcomeText">Pour s'inscrire ou se connecter : <router-link to="/account">Mon Compte</router-link>
-      <br>
-
-      <br>
-      Pour accéder au forum : <router-link to="/forum">Forum</router-link></p>
-      
-     
-    </main>
-    <footer class="footer">
-      {{ copyright }}
-    </footer>
+    <h1>Bienvenue <br />dans le réseau social de votre societé</h1>
+    <p class="welcomeText">
+      Pour s'inscrire ou se connecter :
+      <router-link to="/account">Mon Compte</router-link>
+      <br />
+      <br />
+      Pour accéder au forum :
+      <router-link to="/forum">Forum</router-link>
+    </p>
   </div>
 </template>
 
@@ -26,36 +18,13 @@
 
 
 
-<script>
-  export default {
-    name: 'Home',
-    computed: {
-      copyright() {
-        const currentYear = new Date().getFullYear();
-        return `Copyright Groupomania ${currentYear}`;
-      }
-    }
-  }
-</script>
-
 
 
 
 
 <style>
-.welcomeText {
+.text {
   text-align: center;
   font-weight: 700;
 }
-
-
-
-footer {
-background: red;
-position: absolute;
-bottom: 0;
-left: 0;
-right: 0;
-}
-
 </style>
