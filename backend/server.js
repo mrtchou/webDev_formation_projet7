85ca16d2eea9e3ccd27eccdc7435dc6d52755c30
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-02-12 19:37:55 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,48 @@
-const http = require('http');
-const app = require('./app');
-require('./config/sequelize-config');
+const http = require("http");
+const app = require("./app");
 
-// Définition du port utilisé
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
-// Gestion des différentes erreurs pouvant survenir lors de la connexion au serveur
-const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges.');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use.');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
 };
 
-// Création du serveur
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+//Création du serveur avec la méthode createServer et la fonction app
 const server = http.createServer(app);
-server.on('error', errorHandler);
-server.on('listening', () => {
-    address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
 
-// Démarrage du serveur
-server.listen(process.env.PORT || 3000);
+server.listen(port);
