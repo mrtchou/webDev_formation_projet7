fce1e3e1c30e7f5b66c8652361e31d5b8b9e08a2
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-02-13 17:42:11 +0100

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <router-view/>    
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 
 <style>
-@font-face {
-    font-family: 'montserratregular';
-    src: url('./assets/font/montserrat-regular-webfont.woff2') format('woff2'),
-         url('./assets/font/montserrat-regular-webfont.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
-
 </style>
