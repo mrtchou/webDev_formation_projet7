dca0a6b1c657b30769293a03fe812a6084d7aff4
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-02-14 23:01:43 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,20 +5,27 @@ const path = require("path");
 
 const app = express();
 
+
+
+
+
 const db = require("./models/index");
 db.sequelize.sync({ force: false })
   .then(() => {
-  console.log("Synchronisation de la base de données");
+    console.log("Synchronisation avec la base de données");
   });
 
-app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(cors());//active les en tete
+app.use(bodyParser.json()); //examine les requêtes Content-Type: application/jsonen-tête et transforme en variables accessibles JS sous req.body
+app.use(bodyParser.urlencoded({ extended: true })); //extended: true précise que req.body contiendra valeurs de n'importe quel type
+
+
 
 require("./routes/user")(app);
 require("./routes/post")(app);
+
+
 
 module.exports = app;
