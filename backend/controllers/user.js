6978e24b66702a180c56ab1cb8c0ed15d274d96a
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-02-14 23:03:10 +0100

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,21 +1,28 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const db = require("../models");
-const { secret } = require("../config.json");
-const { oldEmployesPassword } = require("../config.json");
+const { tokenJWT } = require("../config.json");
+const { ancienEmployee } = require("../config.json");
 
 const User = db.user;
 const regexEmail = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/;
+
+
+/**
+ * pour lire les données HTTP POST, utilise "body-parser". 
+ * body-parser est un middleware express qui lit l'entrée d'un formulaire 
+ * et le stocke en tant qu'objet javascript accessible via req.body
+ */
 
 //Création d'un utilisateur
 exports.createUser = (req, res, next) => {
   if (!req.body.email || !req.body.pseudo || !req.body.password) {
     return res.status(400).json({
-      message: "missing data",
+      message: "absence de data dans formulaire",
     });
   } else if (!regexEmail.test(req.body.email)) {
     return res.status(400).json({
-      message: "bad request",
+      message: "Mauvais requette.",
     });
   }
   bcrypt
@@ -25,18 +32,18 @@ exports.createUser = (req, res, next) => {
         email: req.body.email,
         pseudo: req.body.pseudo,
         password: hash,
-        profil_picture: req.body.profil_picture,
         is_admin: req.body.is_admin,
       };
       User.create(user)
         .then(() => {
           res.status(200).json({
-            message: "success",
+            message: "success : Utilisateur a bien ete créé",
           });
         })
         .catch((err) => {
-          if ((err.errors[0].message = "users.email must be unique")) {
-            res.status(409).json({ message: "conflict" });
+          if ((err.errors[0].message = "users.email doit etre unique. ")) {
+            res.status(409).json({ message: "erreur pour la creation utilisateur" });
+
           } else {
             res.status(500).json({ err });
           }
@@ -45,13 +52,21 @@ exports.createUser = (req, res, next) => {
     .catch((err) => res.status(500).json({ err }));
 };
 
+
+
+
+
+
+
+
+
 //Connexion d'un utilisateur existant
 exports.login = (req, res, next) => {
   User.findOne({ where: { email: req.body.email } })
     .then((user) => {
       if (!user) {
         return res.status(401).json({
-          message: "authentification failed",
+          message: "authentification echoué",
         });
       }
       bcrypt
@@ -59,18 +74,26 @@ exports.login = (req, res, next) => {
         .then((valid) => {
           if (!valid) {
             return res.status(401).json({
-              message: "authentification failed",
+              message: "authentification echoué",
             });
           }
           res.status(200).json({
             userId: user.id,
-            token: jwt.sign({ userId: user.id }, secret, { expiresIn: "24h" }),
+            token: jwt.sign({ userId: user.id }, tokenJWT, { expiresIn: "24h" }),
           });
         })
         .catch((err) => res.status(500).json({ err }));
     })
     .catch((err) => res.status(500).json({ err }));
 };
+
+
+
+
+
+
+
+
 
 //Récupération d'un utilisateur
 exports.getOneUser = (req, res, next) => {
@@ -79,12 +102,11 @@ exports.getOneUser = (req, res, next) => {
     .then((user) => {
       if (!user) {
         return res.status(404).json({
-          message: "user not found",
+          message: "Utilisateur n'a pas ete trouvé ou n'existe pas.",
         });
       }
       res.status(200).json({
         admin: user.is_admin,
-        profilePicture: user.profil_picture,
         userId: user.id,
         email: user.email,
         pseudo: user.pseudo,
@@ -92,6 +114,14 @@ exports.getOneUser = (req, res, next) => {
     })
     .catch((err) => res.status(500).json({ err }));
 };
+
+
+
+
+
+
+
+
 
 //Modification d'un utilisateur
 exports.modifyUser = (req, res, next) => {
@@ -107,32 +137,31 @@ exports.modifyUser = (req, res, next) => {
     password,
     is_admin: req.body.is_admin,
   };
-  if (req.file) {
-    user.profil_picture = `${req.protocol}://${req.get("host")}/images/${req.file.filename}`;
-  }
   User.update(user, {
     where: { id: req.params.id },
   })
     .then((data) => {
       if (data[0] === 0) {
         return res.status(404).json({
-          message: "user not found",
+          message: "utilisateur non trouvé",
         });
       } else {
-        res.status(200).json({ message: "user modified" });
+        res.status(200).json({ message: "utilisateur a bien été modifié" });
       }
     })
     .catch((err) => res.status(500).json({ err }));
 };
 
-//Suppression (ou anonymisation) d'un utilisateur
+
+
+//anonymisation utilisateur (regle europeennes RGPD.)
 exports.deleteUser = (req, res, next) => {
   User.update(
     {
-      email: "ancien employé" + Date.now(),
-      pseudo: "ancien employé",
-      password: oldEmployesPassword,
-      profil_picture: "http://localhost:3000/images/avatar.png",
+
+      email: "ancien employé compte supprimé le: " + new Date(),
+      pseudo: "compte supprimé",
+      password: Math.random(),
       is_admin: 0,
     },
     {
