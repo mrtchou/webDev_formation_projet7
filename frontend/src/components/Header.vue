b8605e22bede8ca0458e3468c4bfcdf56388e557
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-02-17 12:58:49 +0100

    Update Header.vue

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,76 +1,298 @@
 <template>
-  <b-container class="mb-4">
-    <b-navbar class="pt-3" toggleable="sm" type="light">
-      <b-navbar-brand class="header-box" @click="reloadPage">
-        <router-link to="/">
-          <img
-            class="logo"
-            src="../assets/images/icon-header.png"
-            alt="Logo and company name"
-          />
-        </router-link>
-      </b-navbar-brand>
-      <b-navbar-toggle
-        class="nav-collapse"
-        target="nav-collapse"
-      ></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item v-show="!displayProfile" @click="switchDisplayProfile"
-            >Mon espace personnel</b-nav-item
+  <b-container display="flex">
+    <Header
+      @display-profile="switchDisplayProfile"
+      :displayProfile="displayProfile"
+    />
+    <Profile
+      @display-profile="switchDisplayProfile"
+      v-show="displayProfile"
+      :userId="userId"
+      :token="token"
+      :displayProfile="displayProfile"
+    />
+
+    <!--Affichage de l'en-tête d'un utilisateur dans nouvel affichage-->
+    <b-row v-show="displayPostByProfile" class="mb-4">
+      <b-col align="center" lg="5">
+        <h1>Profil de {{ userName }}</h1>
+        <p>{{ userEmail }}</p>
+
+        <div>
+          <b-button
+            pill
+            size="sm"
+            class="back-button"
+            @click="
+              switchDisplayPostByProfile(false);
+              getPosts();
+            "
+            >Retour vers le forum</b-button
           >
-          <b-nav-item v-show="displayProfile" @click="switchDisplayProfile"
-            >Retourner sur le Forum</b-nav-item
-          >
-          <b-nav-item @click="logout">Déconnexion</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+        </div>
+      </b-col>
+    </b-row>
+
+    <!--Insertion article-->
+    <div v-show="!displayPostByProfile">
+      <b-row v-show="!displayProfile">
+        <b-col>
+          <b-form-textarea
+            maxlength="1000"
+            id="textarea-rows"
+            placeholder="Partagez un texte qui merite d'etre partagé avec vos collègues!"
+            @input="lenghtCheck"
+            rows="3"
+            class="text-area"
+            v-model="postTextArea"
+          ></b-form-textarea>
+
+          <p class="error-message font-weight-bold text-center mt-2">
+            {{ postError }}
+          </p>
+
+          <div>
+            <b-button
+              size="sm"
+              class="mb-2 bg-primary text-white btn-lg btn-block"
+              @click="createPost"
+              >Poster</b-button
+            >
+            <b-button
+              size="sm"
+              class="mb-3 reset-button btn-lg btn"
+              @click="resetPost"
+              >Effacer tout le texte</b-button
+            >
+          </div>
+        </b-col>
+      </b-row>
+      <b-row
+        v-for="postData in posts"
+        :key="postData.id"
+        v-show="!displayProfile"
+      >
+        <b-col>
+          <Post
+            @users-posts="usersPosts"
+            @post-by-profile="switchDisplayPostByProfile"
+            :post="postData"
+            :admin="admin"
+            :userId="userId"
+            :token="token"
+          ></Post>
+        </b-col>
+      </b-row>
+    </div>
   </b-container>
 </template>
  
 <script>
+import Header from "./Header.vue";
+import Post from "./Post.vue";
+import Profile from "./Profile.vue";
+import { url } from "../main";
 export default {
-  name: "Header",
-  props: {
-    displayProfile: {
-      type: Boolean,
+  name: "Forum",
+  components: {
+    Header,
+    Post,
+    Profile,
+  },
+  data() {
+    return {
+      postError: "",
+      userProfilePicture: "",
+      userName: "",
+      userEmail: "",
+      displayPostByProfile: false,
+      displayCommands: false,
+      displayProfile: false,
+      posts: [],
+      users: [],
+      token: "",
+      userId: "",
+      admin: false,
+      error: {},
+      postTextArea: "",
+      testKey: "",
+    };
+  },
+  watch: {
+    //Surveille les posts dans le cas de rechargement
+    posts() {
+      if (this.posts !== "undefined" && this.posts.length > 0) {
+        //evite erreur si aucun post publié
+        this.userName = this.posts[0].user;
+        if (this.posts[0].email.startsWith("ancien employé")) {
+          this.userEmail = "";
+        } else {
+          this.userEmail = this.posts[0].email;
+        }
+      }
+    },
+    //si affichage du profil, alors cache les articles
+    displayProfile() {
+      if (this.displayProfile === true) {
+        this.displayPostByProfile = false;
+      }
     },
   },
+  computed: {
+    headers() {
+      return { headers: { Authorization: this.token, userId: this.userId } };
+    },
+  },
+  //Validation du state de l'utilisateur
+  created() {
+    this.getUser();
+  },
+  //construction du composant post
+  mounted() {
+    this.getPosts();
+  },
   methods: {
-    switchDisplayProfile() {
-      let emitDisplayProfile = !this.displayProfile;
-      this.$parent.getPosts();
-      this.$emit("display-profile", emitDisplayProfile);
+    lenghtCheck() {
+      if (this.postTextArea.length === 1000) {
+        this.postError = "Votre message est trop long";
+      } else {
+        this.postError = "";
+      }
     },
-    disableDisplayProfile() {
-      this.$emit("display-profile", false);
+    createPost() {
+      if (!this.postTextArea) {
+        this.postError = "Votre publication est vide";
+        setTimeout(() => {
+          this.postError = "";
+        }, 3000);
+        return;
+      }
+      let formData = new FormData();
+      formData.append("content", this.postTextArea);
+      formData.append("user_id", this.userId);
+      this.$http
+        .post(url + "posts", formData, this.headers)
+        .then(() => {
+          this.resetPost();
+          this.getPosts();
+        })
+        .catch(() => {
+          this.postError = "Un problème est survenu, veuillez réessayer";
+        });
     },
-    reloadPage() {
-      location.reload();
+    getPosts() {
+      //utilisée pour charger les posts et pour recharger le composant
+      this.$http.get(url + "posts", this.headers).then((res) => {
+        this.posts = res.data;
+      });
     },
-    logout() {
-      localStorage.clear();
-      this.$router.push("/login");
+    usersPosts(data) {
+      //charger post par utilisateur
+      this.posts = data;
+    },
+    switchDisplayPostByProfile(data) {
+      this.displayPostByProfile = data;
+    },
+    switchDisplayProfile(data) {
+      this.displayProfile = data;
+    },
+    //lancé a created pour vérifier l'authenticité du user et si admin ou pas
+    getUser() {
+      const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+      if (currentUser) {
+        this.token = currentUser.token;
+        this.userId = currentUser.userId;
+        this.$http
+          .get(url + "users/" + currentUser.userId, this.headers)
+          .then((res) => {
+            this.admin = res.data.admin;
+          })
+          .catch(() => {
+            this.$router.push("/login");
+          });
+      } else {
+        this.$router.push("/login");
+      }
+    },
+    resetPost() {
+      this.postTextArea = "";
     },
   },
 };
 </script>
 
-<style scoped>
-.logo {
-  height: 7vh;
+<style>
+body {
+  margin: 5%;
+  font-family: Arial, Helvetica, sans-serif;
 }
-.nav-item {
+</style>
+
+<style scoped>
+.text-area {
+  border: 1px solid#c4f5bd;
+  display: flex;
+  margin-right: auto;
+}
+.text-area:focus {
+  outline: none !important;
+  border: 1px solid #73ec3f;
+  box-shadow: 0 0 10px #ffa5a5;
+}
+.post {
+  border-bottom: 0.15em solid #fd2d01;
+  padding-right: 0;
+  padding-left: 0;
+}
+.send-button {
+  background-color: #6bfaa2;
+  color: black;
+  border: solid 1px #a6fa57;
+}
+.send-button:hover {
+  background: #2eee27;
+}
+.reset-button:hover {
+  background: #ffe4e4;
+}
+.post-header {
+  background-color: #ffd7d7;
+  font-size: 1em;
+  border-radius: 80px 30px;
   text-align: center;
 }
-@media screen and (max-width: 420px) {
-  .logo {
-    width: 100%;
-    height: 100%;
+.back-button {
+  background-color: transparent;
+  border: solid 1px #ffb3b3;
+  color: #e42701;
+}
+.back-button:hover {
+  background: #ffe4e4;
+}
+.button-col {
+  display: flex;
+  width: 7em;
+  flex-direction: column;
+}
+.error-message {
+  color: #fd2d01;
+}
+.breadcrumb {
+  cursor: pointer;
+}
+.breadcrumb:hover {
+  background-color: #ebe0e0;
+}
+@media screen and (max-width: 767px) {
+  .button-col {
+    flex-direction: row-reverse;
+    justify-content: space-evenly;
+    width: auto;
   }
-  .header-box {
-    width: 150px;
+}
+@media screen and (max-width: 560px) {
+  h1 {
+    font-size: 2em;
   }
 }
 </style>
