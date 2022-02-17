419e60c2633fa89b63a6fe25977ad3d9692babef
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-02-17 16:02:29 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 module.exports = {
   //Connexion à MySQL
   HOST: "localhost",
-  user: 'root',
-  password: 'root',
-  database: 'groupomania',
-  dialect: 'mysql',
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+  USER: "root",
+  PASSWORD: "root",
+  DB: "groupomania",
+  dialect: "mysql",
+  PORT: "8889",
+  SOCKET: '/Applications/MAMP/tmp/mysql/mysql.sock',
+  DIALECT: 'mysql',
 };
-
-///!!!!!!!!!a relier au fichier index.js dans models car les données ne doivent pas etre transmis avec le code pour DB
