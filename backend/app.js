d8dbf03e10baa354ecd0670222058315ff24ea11
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-02-21 21:57:30 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-
 const app = express();
-
-
-
-
 
 const db = require("./models/index");
 db.sequelize.sync({ force: false })
@@ -16,12 +11,9 @@ db.sequelize.sync({ force: false })
   });
 
 
-
 app.use(cors());//active les en tete
 app.use(bodyParser.json()); //examine les requêtes Content-Type: application/jsonen-tête et transforme en variables accessibles JS sous req.body
 app.use(bodyParser.urlencoded({ extended: true })); //extended: true précise que req.body contiendra valeurs de n'importe quel type
-
-
 
 require("./routes/user")(app);
 require("./routes/post")(app);
