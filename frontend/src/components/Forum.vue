fcef1208473ff6c26808f4056daef893a9618041
mrtchou <77937111+mrtchou@users.noreply.github.com>
2022-02-21 21:53:43 +0100

    Update Forum.vue

--- frontend/src/components/Forum.vue
+++ frontend/src/components/Forum.vue
@@ -77,7 +77,8 @@
             @users-posts="usersPosts"
             @post-by-profile="switchDisplayPostByProfile"
             :post="postData"
-           :isAdmin="isAdmin"
+            :admin="admin"
+            :userId="userId"
             :token="token"
           ></Post>
         </b-col>
@@ -91,7 +92,6 @@ import Header from "./Header.vue";
 import Post from "./Post.vue";
 import Profile from "./Profile.vue";
 import { url } from "../main";
-
 export default {
   name: "Forum",
   components: {
@@ -99,7 +99,6 @@ export default {
     Post,
     Profile,
   },
-
   data() {
     return {
       postError: "",
@@ -113,14 +112,14 @@ export default {
       users: [],
       token: "",
       userId: "",
-      admin: "",
+      admin: false,
       error: {},
       postTextArea: "",
       testKey: "",
     };
   },
   watch: {
-    //Surveille les posts dans le cas de rechargement
+    //Surveille les posts dans changement/rechargement
     posts() {
       if (this.posts !== "undefined" && this.posts.length > 0) {
         //evite erreur si aucun post publié
@@ -144,35 +143,37 @@ export default {
       return { headers: { Authorization: this.token, userId: this.userId } };
     },
   },
-  //Validation du state de l'utilisateur
+  //Validation du state de l'utilisateur / donc utilisateur vient apres creation
   created() {
     this.getUser();
   },
-  //construction du composant post
+  //construction du composant post une fois que le dom est monté
   mounted() {
     this.getPosts();
   },
-
   methods: {
     lenghtCheck() {
-      if (this.postTextArea.length === 1000) {
-        this.postError = "Votre message est trop long";
+      if (this.postTextArea.length > 1000) {
+        this.postError =
+          "Votre message est trop long. Essayez de resumer en un texte plus court";
       } else {
         this.postError = "";
       }
     },
-
     createPost() {
       if (!this.postTextArea) {
-        this.postError = "Votre publication est vide";
+        this.postError = "Votre publication est vide, et ne peut etre creer. ";
         setTimeout(() => {
           this.postError = "";
         }, 7000);
         return;
       }
+      //FormData sera rempli avec clés/valeurs du formulaire
       let formData = new FormData();
       formData.append("content", this.postTextArea);
       formData.append("user_id", this.userId);
+
+      //envoi par post formData puis vider et vider textArea
       this.$http
         .post(url + "posts", formData, this.headers)
         .then(() => {
@@ -180,7 +181,8 @@ export default {
           this.getPosts();
         })
         .catch(() => {
-          this.postError = "Un problème est survenu, veuillez réessayer";
+          this.postError =
+            "Un problème est survenu au moment de la creation de l'article, veuillez réessayer";
         });
     },
     getPosts() {
@@ -190,9 +192,10 @@ export default {
       });
     },
     usersPosts(data) {
-      //charger post par utilisateur
+      //charger post par utilisateur (function a terminer!)
       this.posts = data;
     },
+    //afficher post par profil, ou post juste
     switchDisplayPostByProfile(data) {
       this.displayPostByProfile = data;
     },
@@ -208,7 +211,7 @@ export default {
         this.$http
           .get(url + "users/" + currentUser.userId, this.headers)
           .then((res) => {
-            this.admin = res.data.admin; //a corriger apres la reglage backend mysql
+            this.admin = res.data.admin;
           })
           .catch(() => {
             this.$router.push("/login");
@@ -226,3 +229,81 @@ export default {
 
 
 
+
+
+<style>
+body {
+  margin: 5%;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+
+<style scoped>
+.text-area {
+  border: 1px solid#d0f0cb;
+  display: flex;
+  margin-right: auto;
+}
+.text-area:focus {
+  outline: none !important;
+  border: 1px solid #ed9b67;
+  box-shadow: 0 0 10px #ffa5a5;
+}
+.post {
+  border-bottom: 0.15em solid #fd2d01;
+  margin: 25px;
+  padding-right: 0;
+  padding-left: 0;
+}
+.send-button {
+  background-color: #6ee4ff;
+  color: black;
+  border: solid 1px #a6fa57;
+}
+.send-button:hover {
+  background: #2eee27;
+}
+.reset-button:hover {
+  background: #ffe4e4;
+}
+.post-header {
+  background-color: #ffd7d7;
+  font-size: 1em;
+  border-radius: 80px 30px;
+  text-align: center;
+}
+.back-button {
+  background-color: transparent;
+  border: solid 1px #ffb3b3;
+  color: #e42701;
+}
+.back-button:hover {
+  background: #ffe4e4;
+}
+.button-col {
+  display: flex;
+  width: 7em;
+  flex-direction: column;
+}
+.error-message {
+  color: #fd2d01;
+}
+.breadcrumb {
+  cursor: pointer;
+}
+.breadcrumb:hover {
+  background-color: #ebe0e0;
+}
+@media screen and (max-width: 767px) {
+  .button-col {
+    flex-direction: row-reverse;
+    justify-content: space-evenly;
+    width: auto;
+  }
+}
+@media screen and (max-width: 560px) {
+  h1 {
+    font-size: 2em;
+  }
+}
+</style>
